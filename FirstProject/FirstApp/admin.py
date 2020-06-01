4a156509f6bf81bc5b942c402385312b0e140696
vishall <65184972+vpatel1816@users.noreply.github.com>
2020-06-01 17:29:20 -0400

    Project 1 FInished
    Created DataBase that consist Name,Eid , COntact#, Address

--- FirstProject/FirstApp/admin.py
+++ FirstProject/FirstApp/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from . models import Info
 
 # Register your models here.
+class Infoo(admin.ModelAdmin):
+	list_display = ('Name','Eid')
+
+admin.site.register(Info, Infoo)
--- FirstProject/FirstApp/models.py
+++ FirstProject/FirstApp/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Info(models.Model):
+	Name = models.CharField(max_length=20)
+	Eid = models.CharField(max_length=8)
+	Contact = models.IntegerField()
+	Address = models.CharField(max_length=100)
+
+	def __str__(self):
+		return self.Name
